099a3a8031f181f2543093d2e536f9979b2f0deb
Mariya Barska <moiweer@gmail.com>
2022-08-03 23:55:07 +0300

    copy hw8

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,7 @@
-// Add imports above this line
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
